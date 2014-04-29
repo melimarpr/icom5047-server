b5d6e525b6e7bebdedcfbfaa246fe6a64809bc57
Jesus Luzon <jesus.e.luzon@gmail.com>
2014-04-29 01:41:38 -0400

    Commiting missing exception.

---
+++ aerobal-server/app/exceptions/InvalidRequestException.java
@@ -0,0 +1,30 @@
+package exceptions;
+
+public class InvalidRequestException extends Exception {
+
+	public InvalidRequestException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public InvalidRequestException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public InvalidRequestException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public InvalidRequestException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public InvalidRequestException(String message, Throwable cause,
+			boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+}
